a003e4ecd54eccdf182ca03a52ccd07f9edb9bf5
Felix0906 <1061235013@qq.com>
2019-08-13 09:35:29 +0800

    second commit

--- UsedCard/settings.py
+++ UsedCard/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+                    os.path.join(BASE_DIR, 'index', 'static'),)
+
+SESSION_COOKIE_AGE = 60*60*24
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
